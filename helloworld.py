5973caaa68540da3475f4f57a9cf461b2e396192
genericewe <henryabraham2001@gmail.com>
2025-02-28 14:10:45 +0000

    Modificaciones de helloworld.py Equipo#2

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,12 @@ print('Hello World!!')
 print('Hola Panama')
 print('Hola Banco General')
 print('Comprobando el funcionamiento del Banco General')
+
+print("El equipo 2 estuvo aqui")
+print('Miguel Uranga')
+print('Jason Nuñez')
+print('Henry Archibold')
+print('Abraham Otero')
+
+#Cuando se realiza el git stash, el archivo modificado se gaurda como si fuese un commit. 
+#Cuando se realiza el git stash pop, entonces el archivo vuelve a mostrarse como modificado sin commit
