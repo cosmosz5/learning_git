0d06a0b622d5bf434f9ba0b5829b87a2e929a321
gemr <gemrdev@gmail.com>
2025-02-28 14:36:43 +0000

    team_5 practica terminada

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Grupo 5:
+# Abraham chen
+# Adriana Sanchez
+# Gilberto Molinar
+# Alexander Camargo
 
 print('Hello World!!')
 print('Hola Panama')
 print('Hola Banco General')
 print('Comprobando el funcionamiento del Banco General')
+
+# Al darle git stash guardamos los cambios de la rama del equipo temporalmente dentro
+# de la cache y me movi a la rama principal, con el git stash pop regrese a donde quede 
+# en la ultima rama guardada en el stash y elimina la rama del stash
