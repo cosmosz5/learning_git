f66554a68c5bee66cd60943f341ebd61acf5f402
cosmosz5 <cosmosz5@gmail.com>
2025-02-28 02:55:55 +0000

    stash comprobado

--- helloworld.py
+++ helloworld.py
@@ -5,4 +5,4 @@ import matplotlib.pyplot as plt
 print('Hello World!!')
 print('Hola Panama')
 print('Hola Banco General')
-print('Comprobando el funcionamiento del Banco Central')
+print('Comprobando el funcionamiento del Banco General')
