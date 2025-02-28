c097f8f7cd155217093dfd430b1f11de0b540a62
aldoapicella <aldoapicella@outlook.com>
2025-02-28 09:42:32 -0500

    Equipo 7: se ha hecho la practica de forma satisfactoria

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+print('Equipo #7\nAldo Apicella\nOrlando Camargo\nAngelica Reyes\n')
+ 
 print('Hello World!!')
 print('Hola Panama')
 print('Hola Banco General')
 print('Comprobando el funcionamiento del Banco General')
+
+print ('Saved working directory and index state WIP on Equipo7: c7a397c Prueba de stash')
+print ('Dropped refs/stash@{0} (3dc2ebd9229c1cd91e7fb6bc83706cbbe958671a)')
