90f24a70258b0cbe2a8c6fbd6d29dab17f0d0998
cosmosz5 <cosmosz5@gmail.com>
2025-02-28 14:13:57 +0000

    adding team members and answering questions

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,14 @@ print('Hello World!!')
 print('Hola Panama')
 print('Hola Banco General')
 print('Comprobando el funcionamiento del Banco General')
+integrantes = ["Ana De Janon", "Carlos Gonzalez", "Carolina Valdes", "Miguel Arrocha"]
+print("Integrantes: ...")
+for i in integrantes:
+    print(i)
+
+# Que le ocurre al archivo helloworld.py cuando se ejecuta el comando git stash
+# si se hace un cambio en la branch main?
+# El archivo helloworld.py que está en main no tiene los cambios que están en el stash.
+# Que le ocurre al archivo helloworld.py cuando se regresa a la branch equipo6
+# y se ejecuta el comando git stash pop?
+# Regresan los cambios guardados temporalmente al archivo helloworld.py
